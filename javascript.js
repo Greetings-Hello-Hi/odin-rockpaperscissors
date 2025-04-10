f97843b2f939e32c42bcef5b401cc7fb71517189
Greetings-Hello-Hi <181512451+Greetings-Hello-Hi@users.noreply.github.com>
2025-04-10 12:40:37 -0500

    Standardize case for function names

--- javascript.js
+++ javascript.js
@@ -1,29 +1,32 @@
-// The RandomInteger function helps randomize the computer choice, returning an integer from 0–8.
-function RandomInteger() {
+// The randomInteger function helps randomize the computer choice, returning an integer from 0–8.
+function randomInteger() {
     return Math.floor(Math.random() * 9);
 }
-let RandomInt = RandomInteger();
+let randomInt = randomInteger();
+
 // The getComputerChoice function converts the random integer into rock, paper, or scissors.
 // 0–2 is rock, 3–5 is paper, and 6–8 is scissors.
-function getComputerChoice(RandomInt) {
-    if (RandomInt <= 2) {
+function getComputerChoice(randomInt) {
+    if (randomInt <= 2) {
         return "rock";
-    } else if (RandomInt <= 5) {
+    } else if (randomInt <= 5) {
         return "paper";
     } else {
         return "scissors";
     }
 }
-let ComputerChoice = getComputerChoice(RandomInt);
+let computerChoice = getComputerChoice(randomInt);
+
 // The humanInput function prompts for input from the human player.
 let humanInput = window.prompt("Rock, paper, or scissors?");
+
 // The getHumanChoice functions standardizes the human player's input to lowercase.
 function getHumanChoice() {
     if (humanInput === null) {
         return "Try again";
     } else {
-    return humanInput.toLowerCase();
+        return humanInput.toLowerCase();
     }
 }
-let HumanChoice = getHumanChoice();
+let humanChoice = getHumanChoice();
 // We can then pit ComputerChoice against HumanChoice.
