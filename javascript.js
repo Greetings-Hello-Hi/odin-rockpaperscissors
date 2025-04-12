60a888ccca8bab4520a9c367de91e2a3ee6fa99e
Greetings-Hello-Hi <181512451+Greetings-Hello-Hi@users.noreply.github.com>
2025-04-11 22:42:35 -0500

    Update comments and remove irrelevant comments

--- javascript.js
+++ javascript.js
@@ -1,196 +1,23 @@
 /*
-// The variables humanScore and computerScore keep track of the players' scores.
-let humanScore = 0;
-let computerScore = 0;
-
-// The randomInteger function helps randomize the computer choice, returning an integer from 0–8.
-function randomInteger() {
-    return Math.floor(Math.random() * 9);
-}
-const randomInt = randomInteger();
-
-// The getComputerChoice function converts the random integer into rock, paper, or scissors.
-// 0–2 is rock, 3–5 is paper, and 6–8 is scissors.
-function getComputerChoice(randomInt) {
-    if (randomInt <= 2) {
-        return "rock";
-    } else if (randomInt <= 5) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
-}
-const computerChoice = getComputerChoice(randomInt);
-
-// The humanInput function prompts for input from the human player.
-const humanInput = window.prompt("Rock, paper, or scissors?");
-
-// The getHumanChoice functions standardizes the human player's correct input to lowercase.
-function getHumanChoice() {
-    if (humanInput === null) {
-        console.log("Try again!");
-    } else {
-        return humanInput.toLowerCase();
-    }
-}
-const humanChoice = getHumanChoice();
-
-// We can then pit computerChoice against humanChoice using the playRound function.
-// The totalRounds variable keeps track of the number of rounds.
-// let totalRounds = 0;
-
-function playRound(human, computer) {
-    if (human == "rock" && computer == "scissors") {
-        console.log("You win! Rock beats Scissors.");
-        humanScore = ++humanScore;
-        totalRounds = ++totalRounds;
-        console.log(`You:${humanScore}, Computer:${computerScore}`);
-    } else if (human == "paper" && computer == "rock") {
-        console.log("You win! Paper beats Rock.");
-        humanScore = ++humanScore;
-        totalRounds = ++totalRounds;
-        console.log(`You:${humanScore}, Computer:${computerScore}`);
-    } else if (human == "scissors" && computer == "paper") {
-        console.log("You win! Scissors beats Paper.");
-        humanScore = ++humanScore;
-        totalRounds = ++totalRounds;
-        console.log(`You:${humanScore}, Computer:${computerScore}`);
-    } else if (human == "rock" && computer == "paper") {
-        console.log("You lose. Paper beats Rock.");
-        computerScore = ++computerScore;
-        totalRounds = ++totalRounds;
-        console.log(`You:${humanScore}, Computer:${computerScore}`);
-    } else if (human == "paper" && computer == "scissors") {
-        console.log("You lose. Scissors beats Paper.");
-        computerScore = ++computerScore;
-        totalRounds = ++totalRounds;
-        console.log(`You:${humanScore}, Computer:${computerScore}`);
-    } else if (human == "scissors" && computer == "rock") {
-        console.log("You lose. Rock beats Scissors.");
-        computerScore = ++computerScore;
-        totalRounds = ++totalRounds;
-        console.log(`You:${humanScore}, Computer:${computerScore}`);
-    } else if (human == computer) {
-        console.log("It's a tie, so try again!");
-        console.log(`You:${humanScore}, Computer:${computerScore}`);
-    } else {
-        console.log("Invalid round.");
-    }
-}
-    */
-
-//playRound(humanChoice, computerChoice);
-
-// The playGame function summons playRound for five non-tied rounds.
-/*
-function playGame(totalRounds) {
-    let humanScore = 0;
-    let computerScore = 0;
-    let playRound = function() {
-        let humanInput = window.prompt("Rock, paper, or scissors?");
-        let getHumanChoice = function(humanInput) {
-            if (humanInput === null) {
-                console.log("Try again!");
-            } else {
-                return humanInput.toLowerCase();
-            }
-        };
-        let randomInteger = function() {
-            return Math.floor(Math.random() * 9);
-        };
-        let randomInt = randomInteger();
-        let getComputerChoice = function(randomInt) {
-            if (randomInt <= 2) {
-                return "rock";
-            } else if (randomInt <= 5) {
-                return "paper";
-            } else {
-                return "scissors";
-            }
-        };
-        if (totalRounds == 5) {
-            if (humanScore > computerScore) {
-                console.log(`You win the game! Your total score of ${humanScore} beat the computer's total score of ${computerScore}!`);
-                //alert('The game has ended. Refresh the page to begin a new game!');
-                return;
-                // return totalRounds = 0;
-            } else if (humanScore < computerScore) {
-                console.log(`You lost the game. The computer's total score of ${computerScore} beat your total score of ${humanScore}.`);
-                //alert('The game has ended. Refresh the page to begin a new game!');
-                return;
-                // return totalRounds = 0;
-            } else {
-                console.log(`It's a tie, somehow!`);
-                return;
-            }
-        } else if (getHumanChoice(humanInput) == "rock" && getComputerChoice(randomInt) == "scissors") {
-            console.log("You win! Rock beats Scissors.");
-            humanScore = ++humanScore;
-//            totalRounds = ++totalRounds;
-            console.log(`You:${humanScore}, Computer:${computerScore}`);
-        } else if (getHumanChoice(humanInput) == "paper" && getComputerChoice(randomInt) == "rock") {
-            console.log("You win! Paper beats Rock.");
-            humanScore = ++humanScore;
-//            totalRounds = ++totalRounds;
-            console.log(`You:${humanScore}, Computer:${computerScore}`);
-        } else if (getHumanChoice(humanInput) == "scissors" && getComputerChoice(randomInt) == "paper") {
-            console.log("You win! Scissors beats Paper.");
-            humanScore = ++humanScore;
-//            totalRounds = ++totalRounds;
-            console.log(`You:${humanScore}, Computer:${computerScore}`);
-        } else if (getHumanChoice(humanInput) == "rock" && getComputerChoice(randomInt) == "paper") {
-            console.log("You lose. Paper beats Rock.");
-            computerScore = ++computerScore;
-//            totalRounds = ++totalRounds;
-            console.log(`You:${humanScore}, Computer:${computerScore}`);
-        } else if (getHumanChoice(humanInput) == "paper" && getComputerChoice(randomInt) == "scissors") {
-            console.log("You lose. Scissors beats Paper.");
-            computerScore = ++computerScore;
-//            totalRounds = ++totalRounds;
-            console.log(`You:${humanScore}, Computer:${computerScore}`);
-        } else if (getHumanChoice(humanInput) == "scissors" && getComputerChoice(randomInt) == "rock") {
-            console.log("You lose. Rock beats Scissors.");
-            computerScore = ++computerScore;
-//            totalRounds = ++totalRounds;
-            console.log(`You:${humanScore}, Computer:${computerScore}`);
-        } else if (getHumanChoice(humanInput) == getComputerChoice(randomInt)) {
-            console.log("It's a tie, so try again!");
-            //totalRounds = ++totalRounds;
-            console.log(`You:${humanScore}, Computer:${computerScore}`);
-        } else {
-            console.log("Invalid round.");
-            //totalRounds = ++totalRounds;
-        }
-    };
-        while (totalRounds <= 5) {
-            if (totalRounds > 5) {
-                break;
-            }
-            playRound();
-            totalRounds++;
-        }
-    }
-
-playGame(0);
-alert('The game has ended. Refresh the page to begin a new game!');
+Within the playRound function:
+    The randomInteger function helps randomize the computer choice, returning an integer from 0–8.
+    The getComputerChoice function converts the random integer into rock, paper, or scissors (0–2 is rock, 3–5 is paper, and 6–8 is scissors).
+    The humanInput function prompts for input from the human player.
+    The getHumanChoice functions standardizes the human player’s input to lowercase.
+    Then the computer’s choice is compared to the human’s choice.
+    Finally, the winner of each round is determined and logged to the console. The winner’s score is incremented by 1.
+If five scored rounds have been played, the playRound function compares the players’s scores and determines the winner of the entire game.
+The playGame function uses a while loop to call playRound for five scored rounds.
 */
 
-// The playGame function summons playRound for five rounds.
-// The variables humanScore and computerScore keep track of the players’ scores.
-// The totalRounds variable keeps track of the number of rounds (tied rounds don’t count as rounds here).
-// Within the playRound function, the randomInteger function helps randomize the computer choice, returning an integer from 0–8.
-// Within the playRound function, the getComputerChoice function converts the random integer into rock, paper, or scissors.
-// Within the playRound function, the humanInput function prompts for input from the human player.
-// Within the playRound function, the getHumanChoice functions standardizes the human player’s input to lowercase.
-// Within the rest of the playRound function, the computer’s choice is compared to the human’s choice.
-// Then, the winner of each round is determined and logged to the console. The winner’s score is incremented by 1.
-// If five scored rounds have been played, the playRound function compares the players’s scores and determines the winner of the entire game.
-// The playGame function uses a while loop to call playRound for five scored rounds.
-
+// The playGame function summons playRound for five scored rounds, using a while loop.
+// Note: Tied rounds don’t count as rounds here.
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+    // The variables humanScore and computerScore keep track of the players’ scores.
     let totalRounds = 0;
+    // The totalRounds variable keeps track of the number of rounds.
     let playRound = function() {
         if (totalRounds == 5) {
             if (humanScore > computerScore) {
